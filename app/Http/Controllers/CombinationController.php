a2ef6a8aa8b7d3073dcdc895fc55a109f2e3c130
Lavanya-peiris <lavanyapeiris97@gmail.com>
2023-12-09 19:01:06 +0530

    add file

---
+++ app/Http/Controllers/CombinationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CombinationController extends Controller
+{
+    public function abc(){
+        return view('student.combinations');
+    }
+}
