2a0fe7ace2434943b30c2c05357e970bfcaad9b5
SanukaImodani <sanukaimodani98@gmail.com>
2023-12-14 10:13:18 +0530

    amal

---
+++ resources/views/student/Registration.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    
+
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card" style="height: 6.6cm; width: 12.5cm; margin-top: 6cm; margin-left: 4cm; margin-right: 4cm;">
+      <div class="card-body">
+        <h5 class="card-header text-center"> Combination Selection </h5>
+        <br> <br> <br> 
+        <a href="{{route('select')}}" class="btn btn-success" style="width: 6.6cm; height: 2.2cm; display: block; margin: 0 auto; font-size: 25px; text-align: center;"> Select </a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card" style="height: 6.6cm; width: 12.5cm; margin-top: 6cm; margin-left: 4cm; margin-right: 4cm;">
+      <div class="card-body">
+        <h5 class="card-header text-center"> Course Unit Registration </h5>
+        <br> <br> <br> 
+        <a href="#" class="btn btn-success" style="width: 6.6cm; height: 2.2cm; display: block; margin: 0 auto; font-size: 25px; text-align: center;"> Register </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ resources/views/student/selectCombination.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+
+<div style="border: 1px solid #ccc; padding: 20px; border-radius: 10px; width: 50%; margin: 2.5cm auto 0; background-color: #f9f9f9;">
+<div class="form-group">
+<h4>Student Number</h4>
+<label for="student_ID"></label>
+<input type="name" class="form-control" id="student_ID" placeholder="SC/0000/00000">
+</div>
+
+<br><br>
+
+
+
+
+<div class="form-group">
+<label for="combination"><h4>Select Your Combination</h4></label>
+<select class="form-control" id="combination">
+  <option>A</option>
+  <option>B</option>
+  <option>C</option>
+</select>
+</div>
+
+<br>
+
+<button type="button" style="background-color: #28a745; color: #fff; padding: 10px 20px; font-size: 16px; border: none; border-radius: 5px;">Submit</button>
+</div>
+    
+</body>
+</html>
