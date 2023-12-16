80c5f8a89bd9e6c01669362988a066181437d93d
SanukaImodani <sanukaimodani98@gmail.com>
2023-12-16 17:37:30 +0530

    Suraj

---
+++ database/migrations/2023_12_16_102058_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name');
+            $table->string('Email')->nullable();
+            $table->string('Level');
+            $table->string('Semester');
+            $table->string('Department');
+            $table->string('Demonstrator')->nullable();
+            $table->string('Message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -40,9 +40,9 @@
             </div>
           </li>
           <li class="nav-item">
-            <a href="pages/apps/chat.html" class="nav-link">
+            <a href="{{route('messages.display')}}" class="nav-link">
               <i class="link-icon" data-feather="message-square"></i>
-              <span class="link-title">Chat</span>
+              <span class="link-title">Messages</span>
             </a>
           </li>
           <li class="nav-item">
